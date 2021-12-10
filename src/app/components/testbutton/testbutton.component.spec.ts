5a22d4677eff9b3e962bbe19e1518feb99d5a29c
arshiyanaz1 <shaanarshiya@gmail.com>
2021-12-10 06:24:10 -0800

    first commit1

---
+++ src/app/components/testbutton/testbutton.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestbuttonComponent } from './testbutton.component';
+
+describe('TestbuttonComponent', () => {
+  let component: TestbuttonComponent;
+  let fixture: ComponentFixture<TestbuttonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestbuttonComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestbuttonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
